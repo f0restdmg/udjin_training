1d02ba830583f42a60baf5293682d9d47db4daf1
Николаев Дмитрий Андреевич <daniko18@mts.ru>
2022-01-23 17:26:37 +0400

    инициализация и стилизация слайдера

---
+++ js/script.js
@@ -0,0 +1,7 @@
+$('.slider').slick({
+    slidesToShow: 7,
+    slidesToScroll: 7,
+    infinite: true,
+    prevArrow: '<button type = "button" class = "slick-prev"></ button>',
+    nextArrow: '<button type = "button" class = "slick-next"></ button>'
+})
